ff063775b18ec2605793ef78afdce0b7550e8427
jeanvianney777 <kingvianney18@gmail.com>
2022-03-15 16:36:19 +0000

    commit

--- src/pages/Inscription.js
+++ src/pages/Inscription.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import "../styles/Connexion.css";
 const Inscription = () => {
   return (
     <div>
       <Navigation />
-      <div>
-        <h1>Inscription</h1>
+      <div class="divlogin">
+        <h1 class="h1prem">
+          INSCRIPTION <hr class="hrTitre" />
+        </h1>
+        <div class="stepDiv">
+          <button class="step1 btfocus">Etape 1</button>
+          <button class="step2 btfocus">Etape 2</button>
+          <button class="step3 btfocus">Etape 3</button>
+        </div>
       </div>
     </div>
   );
